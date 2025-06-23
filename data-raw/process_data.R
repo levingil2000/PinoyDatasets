0df81a7dcc158d93a843b9cfcc22be41e1f3a838
Kuya kevin <kevingilguanzon@gmail.com>
2025-06-23 20:01:28 +0800

    added documentation

---
+++ data-raw/process_data.R
@@ -0,0 +1,6 @@
+library(readr)
+library(usethis)
+
+underagemothersPH2023 <- read_csv("data-raw/PH_underage_mothers.csv")
+PISAMathReadSciCRTSummaryScores <-read_csv("data-raw/PISACrtSummary.csv")
+PISAAccesToDeviceAndInternet <- read_csv("data-raw/PISAStudentPropertiesAccessToDevicesAndInternet.csv")
