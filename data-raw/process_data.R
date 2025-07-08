cd476c6db48fdde620cac841a2180c16b5587a99
Kuya kevin <kevingilguanzon@gmail.com>
2025-07-08 22:31:25 +0800

    added csv and xlsx of admin test PISA

--- data-raw/process_data.R
+++ data-raw/process_data.R
@@ -1,9 +1,10 @@
 library(readr)
 library(usethis)
-
+library(readxl)
 underagemothersPH2023 <- read_csv("data-raw/PH_underage_mothers.csv")
 PISAMathReadSciCRTSummaryScores <-read_csv("data-raw/PISACrtSummary.csv")
 PISAAccesToDeviceAndInternet <- read_csv("data-raw/PISAStudentPropertiesAccessToDevicesAndInternet.csv")
 PISABooksFamilyHighesteducation <- read_csv("data-raw/PISA2022BooksParentsSiblings.csv")
 PISASkippingSchool <- read_csv("data-raw/PISA2022SkippingSchool.csv")
 PISATeacherRelationSafety <-read_csv("data-raw/PISA2022TeacherRelationSafety.csv")
+PISA2022AdminSurvey <- read_xlsx("data-raw/PISA2022AdminTest.xlsx")
